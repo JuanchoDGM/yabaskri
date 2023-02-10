7ba9d57d4a5f6d47f97d3f67a889c18cef61bc69
JuanchoDGM <jdgranadillo9@gmail.com>
2023-02-10 09:28:59 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const guardar = (clave,valor)=> {
+    localStorage.setItem(clave,valor);
+}
+
+let nombreProducto = prompt("Ingrese el nombre del producto a guardar:");
+let producto = parseInt(prompt("Ingresa el código del producto a guardar:"));
+
+guardar(nombreProducto,producto);
+
+if (valor == NaN) {
+    document.write("El código del producto" +producto+ " no es válido");
+}
